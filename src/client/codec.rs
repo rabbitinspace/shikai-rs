7676c4039f3cf8997ae693879ba34e19d1cc47fa
Igor Nikitin <rabbitinspace@icloud.com>
2020-07-11 16:32:11 +1000

    text and link parsing

--- src/client/codec.rs
+++ src/client/codec.rs
@@ -1,17 +1,65 @@
-pub mod response;
 pub mod gemtext;
+pub mod response;
 
 use std::convert::TryFrom;
 
 use async_trait::async_trait;
+use bstr::{ByteSlice, CharIndices};
 use tokio::io::{AsyncRead, AsyncReadExt};
 
-use crate::client::payload::{Response, Status, Header};
+use crate::client::payload::{Header, Response, Status};
 
 #[async_trait]
 pub trait Decoder {
     type Output;
     type Error;
 
-    async fn decode<R: AsyncRead + Unpin + Send>(&mut self, source: &mut R) -> Result<Self::Output, Self::Error>;
+    async fn decode<R: AsyncRead + Unpin + Send>(
+        &mut self,
+        source: &mut R,
+    ) -> Result<Self::Output, Self::Error>;
+}
+
+struct FieldIndices<'b> {
+    chars: CharIndices<'b>,
+}
+
+impl<'b> FieldIndices<'b> {
+    fn new(bytes: &'b [u8]) -> Self {
+        let chars = bytes.char_indices();
+        Self { chars }
+    }
+}
+
+impl Iterator for FieldIndices<'_> {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (start, mut end);
+
+        loop {
+            // find a first non-whitespace character
+            match self.chars.next() {
+                Some((s, e, c)) => {
+                    if !c.is_whitespace() {
+                        start = s;
+                        end = e;
+                        break;
+                    }
+                }
+                None => return None,
+            }
+        }
+
+        // find a last non-whitespace character
+        while let Some((_, e, c)) = self.chars.next() {
+            if !c.is_whitespace() {
+                break;
+            }
+
+            end = e
+        }
+
+        return Some((start, end));
+    }
 }
--- src/client/codec/gemtext.rs
+++ src/client/codec/gemtext.rs
@@ -1,4 +1,5 @@
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
+use bstr::ByteSlice;
 
 pub enum Block {
     Empty,
@@ -10,8 +11,8 @@ pub enum Block {
     Quote(QuoteBlock),
 }
 
-pub trait ContentBlock<'b>: From<&'b mut BytesMut> {
-    fn matches(content: &[u8]) -> bool;
+pub trait ContentBlock<'b>: From<&'b mut Bytes> {
+    fn matches(buf: &Bytes) -> bool;
 }
 
 pub struct TextBlock {
@@ -20,7 +21,7 @@ pub struct TextBlock {
 
 pub struct LinkBlock {
     pub url: Bytes,
-    pub name: Bytes,
+    pub name: Option<Bytes>,
 }
 
 pub struct PreformattedBlock {
@@ -42,17 +43,51 @@ pub struct QuoteBlock {
 }
 
 impl ContentBlock<'_> for TextBlock {
-    fn matches(content: &[u8]) -> bool {
-        !content.is_empty()
+    fn matches(buf: &Bytes) -> bool {
+        !buf.is_empty()
     }
 }
 
-impl From<&mut BytesMut> for TextBlock {
-    fn from(content: &mut BytesMut) -> Self {
-        todo!()
+impl From<&mut Bytes> for TextBlock {
+    fn from(buf: &mut Bytes) -> Self {
+        let line_len = match buf.lines().next() {
+            None => return Self { content: Bytes::new() },
+            Some(line) => line.len(),
+        };
+
+        Self { content: buf.split_to(line_len) }
     }
 }
 
-// fn split_at_line_end(content: &mut BytesMut) -> BytesMut {
+impl ContentBlock<'_> for LinkBlock {
+    fn matches(buf: &Bytes) -> bool {
+        buf.len() > 2 && &buf[..2] == b"=>"
+    }
+}
 
-// }
+impl From<&mut Bytes> for LinkBlock {
+    fn from(buf: &mut Bytes) -> Self {
+        let line = match buf[..2].lines().next() {
+            Some(line) => line,
+            None => return LinkBlock { url: Bytes::new(), name: None }
+        };
+
+        let line_len = line.len();
+        let (link_start, link_end) = match super::FieldIndices::new(line).next() {
+            Some(range) => range,
+            None => (0, line.len()),
+        };
+
+        // drop leading line characters that are not a URL
+        let _ = buf.split_to(2 + link_start);
+
+        let url = buf.split_to(link_end);
+        let name = if line_len > link_end {
+            Some(buf.split_to(line_len))
+        } else {
+            None
+        };
+
+        Self { url, name } 
+    }
+}
